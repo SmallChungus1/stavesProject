3754e9a34ef02a53ad23c2ab415636404205dc91
SmallChungus1 <hanson.l@wustl.edu>
2025-05-09 18:19:36 -0700

    working on onnxruntime routing. Addition of yolo_onnx.py to handle inference with onnx models

--- router.py
+++ router.py
@@ -10,6 +10,9 @@ from yoloPostprocessUtils import crop_pillow_img_from_bbox, draw_detections_on_i
 from fastapi.responses import HTMLResponse, JSONResponse
 import torch
 import pandas as pd
+#entry point for onnx inferences
+from yolo_onnx import YOLO_OnnxRuntime
+
 
 device_name = "cuda" if torch.cuda.is_available() else "mps" if  torch.backends.mps.is_available() else "cpu"
 print(f"using device {device_name}")
@@ -50,6 +53,50 @@ async def count_staves(file: UploadFile,
     #run image thru first stage staves localizer
     print("running through first stage localizer model")
     localizer_detection = staves_localzier_model.predict(image, half=True, max_det=1, device=device_name)
+    bbox_corner_cords = localizer_detection[0].boxes.xyxy.cpu().detach().numpy()
+    bbox_corner_cords = bbox_corner_cords[0]
+    cropped_image = crop_pillow_img_from_bbox(bbox_corner_cords, image)
+
+    #second stage staves counter
+    print("running through second stage staves counter model")
+    counter_detections = staves_counter_model(cropped_image, half=True, max_det=9999, device=device_name, conf=conf_thresh, iou=iou_thresh)
+    counter_detections = counter_detections[0]
+
+    annotated_img, staves_count = draw_detections_on_img(counter_detections, cropped_image)
+
+    #store results to pandas table
+    img_name = file.filename
+    if img_name in table_df["img_name"].values:
+        table_df.loc[table_df["img_name"]==img_name, "count"] = staves_count
+    else:
+        table_df = table_df._append({"img_name": img_name, "count": staves_count}, ignore_index=True)
+    
+    #final img to buffer, send back to index page
+    output_buffer = io.BytesIO()
+    annotated_img.save(output_buffer, format="JPEG")
+    output_buffer.seek(0)
+    
+    return StreamingResponse(output_buffer, media_type="image/jpeg", headers={"staves-count": str(staves_count)})
+
+#running prediction with ONNXRuntime
+@app.post("/predictOnnx/")
+async def count_staves(file: UploadFile,
+                        conf_thresh: float = Form(0.25),
+                        iou_thresh: float = Form(0.60)):
+
+    global table_df
+    image_bytes = await file.read()
+    #this is needed for pillow
+    image_stream = io.BytesIO(image_bytes)
+    image = Image.open(image_stream)
+    image.load()
+
+    onnx_localizer_path = ""
+    onnx_counter_path = ""
+
+    #run image thru first stage staves localizer
+    print("running through first stage localizer model")
+    localizer_detection = YOLO_OnnxRuntime(onnx_localizer_path, image, confidence_thres=conf_thresh, iou_thres=iou_thresh)
     bbox_corner_cords = localizer_detection[0].boxes.xyxy.cpu().detach().numpy()
     bbox_corner_cords = bbox_corner_cords[0]
     cropped_image = crop_pillow_img_from_bbox(bbox_corner_cords, image)
